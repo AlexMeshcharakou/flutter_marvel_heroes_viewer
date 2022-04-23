27fadf5b9c6bd6cc13b3ed1639acd312db5507c0
Aliaksei_Meshcharakou <alex84grodno@mail.ru>
2022-04-23 14:18:28 +0300

    add second bloc and DetailsPage_2

--- lib/bloc/heroes_bloc.dart
+++ lib/bloc/heroes_bloc.dart
@@ -2,38 +2,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marvel/bloc/marvel_event.dart';
 import 'package:marvel/bloc/marvel_state.dart';
 import 'package:marvel/data/model/character.dart';
-import 'package:marvel/data/model/series.dart';
 import 'package:marvel/data/repository/marvel_repository.dart';
 
 class HeroesBloc extends Bloc<MarvelEvent, MarvelState> {
   final MarvelRepository marvelRepository;
-   // late List<Character> loadedCharacters;
 
   HeroesBloc(this.marvelRepository)
       : super(CharactersLoadedState(loadedCharacters: [])) {
     on<CharactersLoadEvent>(
-          (event, emit) async {
+      (event, emit) async {
         emit(DataLoadingState());
         try {
           final List<Character> _loadedCharacterList =
-          await marvelRepository.getAllCharacters();
+              await marvelRepository.getAllCharacters();
           emit(CharactersLoadedState(loadedCharacters: _loadedCharacterList));
         } catch (e) {
           emit(MarvellErrorState());
         }
       },
     );
-    // on<DetailsLoadEvent>(
-    //       (event, emit) async {
-    //     emit(DataLoadingState());
-    //     try {
-    //       final List<Series> _loadedDetailList =
-    //       await marvelRepository.getAllSeries(event.characterId);
-    //       emit(DetailsLoadedState(loadedAllSeries: _loadedDetailList));
-    //     } catch (e) {
-    //       emit(MarvellErrorState());
-    //     }
-    //   },
-    // );
   }
 }
