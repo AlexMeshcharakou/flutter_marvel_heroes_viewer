429034bd1016942920fb1ac910e911347a766de8
Aliaksei_Meshcharakou <alex84grodno@mail.ru>
2022-07-15 22:49:05 +0300

    doesnt work

---
+++ domain/lib/domain_module.dart
@@ -0,0 +1,9 @@
+library domain;
+
+export 'src/entities/character.dart';
+export 'src/entities/series.dart';
+export 'src/exceptions/exceptions.dart';
+export 'src/repository/marvel_repository.dart';
+export 'src/use_cases/get_character_details_use_case.dart';
+export 'src/use_cases/get_characters_use_case.dart';
+export 'src/use_cases/get_series_use_case.dart';
