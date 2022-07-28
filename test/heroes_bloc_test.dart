600b618a97fd9fd33f866984ffaf5917dc404a99
Aliaksei_Meshcharakou <alex84grodno@mail.ru>
2022-07-28 09:50:59 +0300

    only heroes_bloc_test

--- test/heroes_bloc_test.dart
+++ test/heroes_bloc_test.dart
@@ -1,13 +1,11 @@
 import 'package:domain/domain_module.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:marvel/presentation/features/heroes/bloc/heroes_bloc.dart';
 import 'package:marvel/presentation/features/heroes/bloc/heroes_event.dart';
 import 'package:marvel/presentation/features/heroes/bloc/heroes_state.dart';
 import 'package:marvel/presentation/view_data/character_view_data.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MockGetCharactersUseCase extends Mock implements GetCharactersUseCase {}
 
@@ -18,16 +16,17 @@ void main() {
 
   blocTest<HeroesBloc, HeroesState>(
     'emits [HeroesState(loading: true, hasReachedMax: false), '
-        'HeroesState(loading: false, hasReachedMax: false, charactersViewData: <CharacterViewData>[])] when successful',
+    'HeroesState(loading: false, hasReachedMax: false, charactersViewData: <CharacterViewData>[])] when successful',
     setUp: () {
       getCharactersUseCase = MockGetCharactersUseCase();
       heroesBloc = HeroesBloc(getCharactersUseCase: getCharactersUseCase);
       when(() => getCharactersUseCase.call()).thenAnswer((_) => testCharacters);
     },
     build: () => heroesBloc,
-    act: (bloc) => bloc..add(ReadyForDataEvent())..add(ScrolledToEndEvent()),
-    expect: () =>
-    [
+    act: (bloc) => bloc
+      ..add(ReadyForDataEvent())
+      ..add(ScrolledToEndEvent()),
+    expect: () => [
       const HeroesState(loading: true, hasReachedMax: false),
       const HeroesState(loading: false, hasReachedMax: false, charactersViewData: <CharacterViewData>[]),
       const HeroesState(loading: true, hasReachedMax: false, charactersViewData: <CharacterViewData>[]),
